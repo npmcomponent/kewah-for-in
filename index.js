da97fd1bb94d48d2b853640cd8ab460e6ca9b2a4
Antoine Lehurt <keuwah@gmail.com>
2013-03-23 21:45:33 +0100

    Improve performance

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-var hasOwnProp = Object.prototype.hasOwnProperty;
+var has = Object.prototype.hasOwnProperty;
 
 /**
- * Iterates an object
+ * Iterates an object.
+ * If the iterator return false, it breaks the loop.
  *
  * @param {Object} obj
  * @param {Function} fn   Iterator
- * @param {Object} ctx    Context
+ * @param {Object} ctx    [Optional] Context
  * @return {Object}
  */
 module.exports = function forIn(obj, fn, ctx) {
   var key;
-  for (key in obj) {
-    if (hasOwnProp.call(obj, key)) {
-      if (false === fn.call(ctx, obj[key], key, obj)) {
-        return obj;
+
+  if (!ctx) {
+    for (key in obj) {
+      if (has.call(obj, key)) {
+        if (false === fn(obj[key], key, obj)) {
+          return obj;
+        }
+      }
+    }
+  } else {
+    for (key in obj) {
+      if (has.call(obj, key)) {
+        if (false === fn.call(ctx, obj[key], key, obj)) {
+          return obj;
+        }
       }
     }
   }
